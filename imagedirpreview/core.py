2b08936d404768c003ef9fc31a53f6349fb31eee
Carsten Knoll <carsten.knoll@posteo.de>
2018-03-29 18:48:04 +0200

    add docstrings and ohter small changes to make pylint happy

--- imagedirpreview/core.py
+++ imagedirpreview/core.py
@@ -4,22 +4,27 @@
 This script serves to generate a browser based preview of all images in a directory.
 """
 
-import jinja2
 from functools import partial
-import inspect
 import sys
 import os
+import jinja2
 
 IMAGE_EXTENSIONS = [".svg", ".png", ".jpg", ".jpeg", ".gif", ".bmp"]
 RESULT_FNAME = "000_preview.html"
 
+
 def get_template_path():
+    """
+    Find out the path of the template. This is not trivial if installed
+    via pip.
+    """
+
     mod = sys.modules.get(__name__)
     if mod is not None and hasattr(mod, '__file__'):
         modpath = os.path.dirname(os.path.abspath(mod.__file__))
         templatepath = os.path.join(modpath, "templates", "base.html")
     else:
-        msg = "Could not find the path of the module".fomat(__name__)
+        msg = "Could not find the path of the module: {}".format(__name__)
         raise ValueError(msg)
 
     if not os.path.isfile(templatepath):
@@ -30,6 +35,9 @@ def get_template_path():
 
 
 def render(tpl_path, context):
+    """
+    Renders the template at tpl_path with the context (dict-like)
+    """
     # source: http://matthiaseisen.com/pp/patterns/p0198/
     path, filename = os.path.split(tpl_path)
     return jinja2.Environment(
@@ -38,14 +46,22 @@ def render(tpl_path, context):
 
 
 def is_image_file(path, fname):
+    """
+    Determine if the file is an image
+    """
     if not os.path.isfile(os.path.join(path, fname)):
         return False
 
     for ext in IMAGE_EXTENSIONS:
         if fname.lower().endswith(ext):
             return True
+    return False
+
 
 def get_all_image_fnames(path="."):
+    """
+    Return a list of file names: images in current directory
+    """
     dircontent = os.listdir(path)
 
     imagelist = sorted(filter(partial(is_image_file, path), dircontent))
@@ -53,11 +69,11 @@ def get_all_image_fnames(path="."):
     return list(imagelist)
 
 
-
-#app = Flask(__name__)
-#@app.route('/')
-
 def main():
+    """
+    Main function. serves as entry point for the bash script.
+    See setup.py
+    """
 
     dirpath = os.path.abspath(os.path.curdir)
     imglist = get_all_image_fnames()
@@ -65,22 +81,18 @@ def main():
     dbg = dirpath
     dbg += "\nlen={}".format(len(imglist))
 
-
-    #url = url_for('REPLACEME', filename=imglist[0])
-    #tmp = '<img src="{}">'.format(url)
-    #dbg += "\n<br>url={}".format(tmp)
-
     templatepath = get_template_path()
     context = dict(imglist=imglist, dirpath=dirpath, dbg=dbg)
 
     res = render(templatepath, context)
-    #res = ""
+    # res = ""
 
     with open(RESULT_FNAME, "w") as resfile:
-       resfile.write(res)
+        resfile.write(res)
 
     print("file written: {}".format(RESULT_FNAME))
 
+
 if __name__ == "__main__":
     pass
-    #main()
+    # main()
