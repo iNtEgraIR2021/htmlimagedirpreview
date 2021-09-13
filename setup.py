7d57c5b4b9c92a03d953b4541603376ebdf6c56d
Petra Mirelli <80395360+iNtEgraIR2021@users.noreply.github.com>
2021-09-13 10:21:32 +0200

    changed project name and meta informations

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from imagedirpreview.release import __version__
+from htmlimagedirpreview.release import __version__
 
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read()
 
 setup(
-    name='imagedirpreview',
+    name='htmlimagedirpreview',
     version=__version__,
     author='Carsten Knoll',
-    packages=['imagedirpreview'],
-    package_data={'imagedirpreview': ['templates/*']},
-    url='https://github.com/cknoll/imagedirpreview',
+    packages=['htmlimagedirpreview'],
+    package_data={'htmlimagedirpreview': ['templates/*']},
+    url='https://github.com/iNtEgraIR2021/htmlimagedirpreview/',
     license='BSD3',
     description='Script for previewing the image-content of directories on the file system',
     long_description="""
     Script for previewing the image-content of directories on the file system.
     Generates a html outputfile at current directory.
+    
+    Based on `imagedirpreview` under copyright by Carsten Knoll.
     """,
     keywords='image preview, helper script',
     install_requires=requirements,
-    entry_points={'console_scripts': ['imagedirpreview=imagedirpreview:main']}
+    entry_points={'console_scripts': ['htmlimagedirpreview=htmlimagedirpreview:main']}
 )
